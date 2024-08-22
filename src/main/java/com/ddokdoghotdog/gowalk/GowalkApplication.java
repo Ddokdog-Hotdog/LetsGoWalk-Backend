a2fc5f3e6363f9609b9230c216c8f4d420eae1f1
KIMCHANGHWAN <98susckdghks@naver.com>
2024-08-22 11:42:53 +0900

    init 초기설정 수정

--- src/main/java/com/ddokdoghotdog/gowalk/GowalkApplication.java
+++ src/main/java/com/ddokdoghotdog/gowalk/GowalkApplication.java
@@ -2,6 +2,8 @@ package com.ddokdoghotdog.gowalk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class GowalkApplication {
@@ -10,4 +12,8 @@ public class GowalkApplication {
 		SpringApplication.run(GowalkApplication.class, args);
 	}
 
+	@Configuration
+	@EnableJpaAuditing // JPA Auditing 활성화
+	public class JpaConfig {
+	}
 }
